3ac12385cc6c113432fca1f652d420c8c5b67625
Vor <sketchpunk@gmail.com>
2018-04-26 15:19:07 -0400

    Added experiments folder and updated vec2

--- fungi/maths/Vec2.js
+++ fungi/maths/Vec2.js
@@ -1,111 +1,133 @@
 class Vec2 extends Float32Array{
 	constructor(ini){
 		super(2);
-		if((ini && ini.length == 2)){
-			this[0] = ini[0]; this[1] = ini[1];
-		}else if(arguments.length == 2){
-			this[0] = arguments[0]; this[1] = arguments[1];
-		}else{
-			this[0] = this[1] = this[2] = ini || 0;
-		}
 
+		if(ini instanceof Vec2 || (ini && ini.length == 2)){	this[0] = ini[0];		this[1] = ini[1]; }
+		else if(arguments.length == 2){ 						this[0] = arguments[0];	this[1] = arguments[1]; }
+		else{													this[0] = this[1] = ini || 0; }
 	}
 
 	//----------------------------------------------
-	// XYZ Setters
-	set(x,y){ this[0] = x; this[1] = y; return this;}
+	//Getters and Setters
+		get x(){ return this[0]; }	set x(val){ this[0] = val; }
+		get y(){ return this[1]; }	set y(val){ this[1] = val; }
+		set(x,y){ this[0] = x; this[1] = y; return this;}
 
-	get x(){ return this[0]; }	set x(val){ this[0] = val; }
-	get y(){ return this[1]; }	set y(val){ this[1] = val; }
+		clone(){ return new Vec2(this); }
+		copy(v){ this[0] = v[0]; this[1] = v[1]; return this; }
+
+		//When values are very small, like less then 0.000001, just make it zero.
+		nearZero(){
+			if(Math.abs(this[0]) <= 1e-6) this[0] = 0;
+			if(Math.abs(this[1]) <= 1e-6) this[1] = 0;
+			return this;
+		}	
+	//endregion
 
 
 	//----------------------------------------------
 	// Methods
-	length(v){
-		//Only get the magnitude of this vector
-		if(v === undefined) return Math.sqrt( this[0]*this[0] + this[1]*this[1] );
+		length(v){
+			//Only get the magnitude of this vector
+			if(v === undefined) return Math.sqrt( this[0]*this[0] + this[1]*this[1] );
 
-		//Get magnitude based on another vector
-		var x = this[0] - v[0],
-			y = this[1] - v[1];
+			//Get magnitude based on another vector
+			var x = this[0] - v[0],
+				y = this[1] - v[1];
 
-		return Math.sqrt( x*x + y*y );
-	}
-	
-	lengthSqr(v){ //TODO Rename to LengthSqr
-		//Only get the squared magnitude of this vector
-		if(v === undefined) return this[0]*this[0] + this[1]*this[1];
+			return Math.sqrt( x*x + y*y );
+		}
+		
+		lengthSqr(v){
+			//Only get the squared magnitude of this vector
+			if(v === undefined) return this[0]*this[0] + this[1]*this[1];
 
-		//Get squared magnitude based on another vector
-		var x = this[0] - v[0],
-			y = this[1] - v[1];
+			//Get squared magnitude based on another vector
+			var x = this[0] - v[0],
+				y = this[1] - v[1];
 
-		return x*x + y*y;
-	}
+			return x*x + y*y;
+		}
 
-	normalize(out){
-		var mag = Math.sqrt( this[0]*this[0] + this[1]*this[1] );
-		if(mag == 0) return this;
+		normalize(out){
+			var mag = Math.sqrt( this[0]*this[0] + this[1]*this[1] );
+			if(mag == 0) return this;
 
-		out = out || this;
-		out[0] = this[0] / mag;
-		out[1] = this[1] / mag;
+			out = out || this;
+			out[0] = this[0] / mag;
+			out[1] = this[1] / mag;
 
-		return this;
-	}
+			return this;
+		}
 
-	scale(v,out){
-		out = out || this;
-		out[0] = this[0] * v;
-		out[1] = this[1] * v;
-		return this;
-	}
+		lerp(v, t, out){
+			if(out == null) out = this;
+			var tMin1 = 1 - t;
 
-	mul(v,out){
-		out = out || this;
-		out[0] = this[0] * v[0];
-		out[1] = this[1] * v[1];
-		return this;
-	}
+			//Linear Interpolation : (1 - t) * v0 + t * v1;
+			out[0] = this[0] * tMin1 + v[0] * t;
+			out[1] = this[1] * tMin1 + v[1] * t;
+			return out;
+		}
 
-	add(v,out){
-		out = out || this;
-		out[0] = this[0] + v[0];
-		out[1] = this[1] + v[1];
-		return this;
-	}
+		rotate(ang, out){
+			if(out == null) out = this;
 
-	sub(v,out){
-		out = out || this;
-		out[0] = this[0] - v[0];
-		out[1] = this[1] - v[1];
-		return this;
-	}
+			var cos = Math.cos(ang),
+				sin = Math.sin(ang),
+				x = this[0],
+				y = this[1];
 
-	div(v,out){
-		out = out || this;
-		out[0] = (v[0] != 0)? this[0] / v[0] : 0;
-		out[1] = (v[1] != 0)? this[1] / v[1] : 0;
-		return this;
-	}
+			out[0] = x * cos - y * sin;
+			out[1] = x * sin + y * cos;
+			return out;
+		}
+	//endregion
 
-	divInvScale(v,out){
-		out = out || this;
-		out[0] = (this[0] != 0)? v / this[0] : 0;
-		out[1] = (this[1] != 0)? v / this[1] : 0;
-		return this;
-	}	
+	//----------------------------------------------
+	// Math
+		add(v, out=null){
+			out = out || this;
+			out[0] = this[0] + v[0];
+			out[1] = this[1] + v[1];
+			return this;
+		}
 
-	clone(){ return new Vec2().set(this.x,this.y); }
-	
-	copy(v){ this[0] = v[0]; this[1] = v[1]; return this; }
+		sub(v, out=null){
+			out = out || this;
+			out[0] = this[0] - v[0];
+			out[1] = this[1] - v[1];
+			return this;
+		}
 
-	scalar(s,out){
-		out = out || this;
-		out[0] = this[0] * s;
-		out[1] = this[1] * s;
-		return this;
-	}
+		mul(v, out=null){
+			out = out || this;
+			out[0] = this[0] * v[0];
+			out[1] = this[1] * v[1];
+			return this;
+		}
+
+		div(v, out=null){
+			out = out || this;
+			out[0] = (v[0] != 0)? this[0] / v[0] : 0;
+			out[1] = (v[1] != 0)? this[1] / v[1] : 0;
+			return this;
+		}
+
+		scale(v, out=null){
+			out = out || this;
+			out[0] = this[0] * v;
+			out[1] = this[1] * v;
+			return this;
+		}
+
+		divInvScale(v, out=null){
+			out = out || this;
+			out[0] = (this[0] != 0)? v / this[0] : 0;
+			out[1] = (this[1] != 0)? v / this[1] : 0;
+			return this;
+		}
+	//endregion
 }
 
 export default Vec2;
