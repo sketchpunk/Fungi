da2e59523bcdde36e814d810373cd88fb14a352d
Vor <sketchpunk@gmail.com>
2018-07-04 01:42:52 -0400

    Fixed Quad and Added another template

--- fungi/primitives/Quad.js
+++ fungi/primitives/Quad.js
@@ -1,16 +1,13 @@
+import Fungi		from "../Fungi.js";
 import Vao 			from "../Vao.js";
-import Renderable	from "../rendering/Renderable.js";
 
 //Define quad with a Min Bound and Max Bound X,Y
 function Quad(matName, name="Quad", bx0=-1, by0=-1, bz0=0, bx1=1, by1=1, bz1=0, stand=true){
-	var d = Quad.vertData(bx0, by0, bz0, bx1, by1, bz1, stand);
-	
-	var vao 	= Vao.standardRenderable(name, 3, d.vertices, null, d.uv, d.index),
-		entity	= new Renderable(name, vao, matName);
-	
-	entity.name = name;
-	entity.options.cullFace = false;
-	return entity;
+	var e = Fungi.ecs.newAssemblage("Draw",name);
+	e.com.Drawable.vao 		= Quad.vao( name="Quad", bx0, by0, bz0, bx1, by1, bz1, stand);
+	e.com.Drawable.material	= Fungi.getMaterial(matName);
+	e.com.Drawable.options.cullFace = false;
+	return e;
 }
 
 Quad.vertData = function(bx0=-1, by0=-1, bz0=0, bx1=1, by1=1, bz1=0, stand=true){
